0376f6c2a658f69c1ff8d44dcdda9b4bb7ddcd72
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-02-19 18:41:57 -0500

    Add setup for running tests.

---
+++ tests/DAL.test.js
@@ -0,0 +1,8 @@
+import { DAL } from '../src/DAL.js';
+
+describe('DAL', () => {
+  it('sets the name correctly', () => {
+    const dalInstance = new DAL("name");
+    expect(dalInstance.name).toBe("name");
+  });
+});
