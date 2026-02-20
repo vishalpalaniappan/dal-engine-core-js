9b0023208266cc35b598d4752b61c091da1a9174
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-02-20 13:51:29 -0500

    Add DALEngine errors.

---
+++ src/Errors/InvalidTransitionError.js
@@ -0,0 +1,12 @@
+import DALEngineError from "./DALEngineError";
+class InvalidTransitionError extends DALEngineError {
+  constructor(from, to) {
+    super(`Invalid transition from "${from}" to "${to}"`);
+
+    this.name = "InvalidTransitionError";
+    this.prevBehavior = from;
+    this.nextBehavior = to;
+  }
+}
+
+export default InvalidTransitionError;
