a0638542c4cfe28aa57d3d93a791843119553e05
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-02-21 09:22:28 -0500

    Add function to add invariant to participant.

--- src/Members/Participant.js
+++ src/Members/Participant.js
@@ -1,5 +1,6 @@
 import Base from "../Base";
 import ENGINE_TYPES from "../TYPES";
+import Invariant from "./Invariant";
 /**
  * Class representing a participant in the design.
  */
@@ -12,6 +13,15 @@ class Participant extends Base{
         super();
         this.name = name;
         this.type = ENGINE_TYPES.PARTICIPANT;
+    }
+
+    /**
+     * Adds an invariant to a participant
+     */
+    addInvariant(name) {
+        const invariant = new Invariant(name);
+        this.invariants.push(invariant);
+        return invariant;
     }
 }
 
